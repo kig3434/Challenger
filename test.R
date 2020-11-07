f6e2c180413ece431311dca29831a38c159409c4
kig3434 <kig3434@aol.com>
2020-11-07 14:05:25 -0700

    a

--- test.R
+++ test.R
@@ -1,5 +1,28 @@
-launch <- read.csv("~/Desktop/Portfolio/Challengervc/data/challenger.csv")
-
 library(stats)
+library(psych)
+library(tidyverse)
+
+
+launch <- read.csv("~/Desktop/Portfolio/Challengervc/docs/challenger.csv")
+summary(launch)
+
+#Regression analyses
+lm1 <- lm(distress_ct ~ temperature, data = launch)
 lm2 <- lm(distress_ct ~ temperature + pressure, data = launch)
-lm2
+lm3 <- lm(distress_ct ~ temperature * pressure, data = launch)
+summary(lm1)
+summary(lm2)
+summary(lm3)
+anova(lm1, lm2, lm3)
+
+
+#Plots
+launch$distress_ct <- as.factor(launch$distress_ct)
+launch$pressure <- as.factor(launch$pressure)
+
+ggplot(launch, aes(x=distress_ct, y=temperature)) + 
+  geom_point(aes(shape=pressure, col=pressure)) +
+  labs(y="Temperature (F)", 
+       x="Number of distressed o-rings", 
+       title="Number of distressed o-rings by temperature", 
+       caption = "Source: challenger.csv")

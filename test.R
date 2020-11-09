9fa722f41c3f4470bf07685af454f758558f5934
kig3434 <kig3434@aol.com>
2020-11-09 13:32:16 -0700

    Complete

--- test.R
+++ test.R
@@ -1,12 +1,34 @@
+# load packages
 library(stats)
 library(psych)
 library(tidyverse)
+library(ggthemes)
 
-
+#Load data
 launch <- read.csv("~/Desktop/Portfolio/Challengervc/docs/challenger.csv")
+
+#Summary stats
 summary(launch)
 
-#Regression analyses
+#Basic Plot#####
+launch$distress_ct <- as_factor(launch$distress_ct)
+launch$pressure <- as_factor(launch$pressure)
+
+ggplot(launch, aes(x = distress_ct, y = temperature)) + 
+  geom_point(aes(shape = pressure, color = pressure), position = position_jitterdodge(dodge.width=0.4), size = 3) +
+  labs(y="Temperature (˚F)", 
+       x="Number of distressed o-rings(out of 6)", 
+       title="Number of distressed o-rings by temperature", 
+       caption = "Source: challenger.csv",
+       color = "Pressure",
+       shape = "Pressure") +
+  theme_calc() +
+  theme(plot.title = element_text(hjust = 0.5))
+
+#Regression analyses####
+launch$distress_ct <- as.numeric(as.character(launch$distress_ct))
+launch$pressure <- as.numeric(as.character(launch$pressure))
+
 lm1 <- lm(distress_ct ~ temperature, data = launch)
 lm2 <- lm(distress_ct ~ temperature + pressure, data = launch)
 lm3 <- lm(distress_ct ~ temperature * pressure, data = launch)
@@ -15,14 +37,13 @@ summary(lm2)
 summary(lm3)
 anova(lm1, lm2, lm3)
 
+## Prediction with simple lm model
+predict(lm1, data.frame(temperature = 36))
 
-#Plots
-launch$distress_ct <- as.factor(launch$distress_ct)
-launch$pressure <- as.factor(launch$pressure)
+# Given the dataset only has 23 observations, there is no need to run a more complex alogrithm that requires training, validation and test datasets.
 
-ggplot(launch, aes(x=distress_ct, y=temperature)) + 
-  geom_point(aes(shape=pressure, col=pressure)) +
-  labs(y="Temperature (F)", 
-       x="Number of distressed o-rings", 
-       title="Number of distressed o-rings by temperature", 
-       caption = "Source: challenger.csv")
+
+
+
+
+
